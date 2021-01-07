4824c6a3ac39e0aacb472dbd7a2724c6ecbc7c29
zhangjinqiu03 <zhangjinqiu03@58.com>
2021-01-07 17:43:25 +0800

    init 示例，及各种初始化的顺序

--- spring-mvc-ssm/src/main/java/com/qiu/init/Init.java
+++ spring-mvc-ssm/src/main/java/com/qiu/init/Init.java
@@ -1,35 +1,17 @@
 package com.qiu.init;
 
+import com.qiu.filter.RequestFilter;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.ServletContextAware;
 
-import javax.servlet.ServletContext;
 @Component
-public class Init implements ApplicationContextAware, ServletContextAware,
-        InitializingBean, ApplicationListener<ContextRefreshedEvent> {
-    @Override
-    public void afterPropertiesSet() throws Exception {
-
-    }
-
+public class Init implements ApplicationContextAware {
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-
+        System.out.println("init");
+        RequestFilter.initLog();
     }
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-
-    }
-
-    @Override
-    public void setServletContext(ServletContext servletContext) {
-
-    }
 }
